7f63b7eb852525207c6657559ff220f8f48a61a7
Ying <41935296+bingying0me@users.noreply.github.com>
2023-11-25 19:18:21 -0500

    Update Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,5 @@
 // src/components/Project.js
-import React from 'react';
+import React from "react";
 
 const Project = () => {
   return (
@@ -7,16 +7,57 @@ const Project = () => {
       <h1 className="container-title">PROJECT</h1>
       <div className="content">
         <div className="content-list-padding project">
+          <div>
+            <img
+              className="project-image"
+              src="/images/charityfinder.png"
+              alt="charityfinder"
+            />
+          </div>
+          <div className="project-content">
+            <h3>Charity Finder</h3>
+            <div>NOV 2023</div>
+            <br />
+            <div>Website: HTML, CSS, React, TypeScript, Vite</div>
+          </div>
+          <div className="project-link">
+            <a href="https://github.com/bingying0me/charityfinderapplication">
+              <img
+                className="project-logo"
+                src="/images/github.png"
+                alt="github"
+              />
+            </a>
+            <a href="https://charityfinderapplication-nine.vercel.app/">
+              <img
+                className="project-logo"
+                src="/images/web.png"
+                alt="github"
+              />
+            </a>
+          </div>
+        </div>
+        <div className="content-list-padding project">
           <div className="flex-display">
             <div>
-              <img className="project-image" src="/images/ShoPair2.png" alt="ShoPair2" />
+              <img
+                className="project-image"
+                src="/images/ShoPair2.png"
+                alt="ShoPair2"
+              />
             </div>
             <div className="project-image-margin">
-              <img className="project-image" src="/images/ShoPair1.png" alt="ShoPair1" />
+              <img
+                className="project-image"
+                src="/images/ShoPair1.png"
+                alt="ShoPair1"
+              />
             </div>
           </div>
           <div className="project-content">
-            <h3>Develop a Comprehensive Product Price APP with Admin Website</h3>
+            <h3>
+              Comprehensive Product Price APP with Admin Website
+            </h3>
             <div>JAN 2019 - MAY 2020</div>
             <br />
             <div>
@@ -28,7 +69,11 @@ const Project = () => {
           </div>
           <div className="project-link">
             <a href="https://github.com/bingying0me/FYP-Web-Server-and-Mobile-App">
-              <img className="project-logo" src="/images/github.png" alt="github" /> 
+              <img
+                className="project-logo"
+                src="/images/github.png"
+                alt="github"
+              />
             </a>
           </div>
         </div>
@@ -37,16 +82,18 @@ const Project = () => {
             <img className="project-image" src="/images/HKPA.png" alt="HKPA" />
           </div>
           <div className="project-content">
-            <h3>Develop a website tailored specifically for HKPA</h3>
+            <h3>HKPA Website</h3>
             <div>JAN 2018 - MAY 2018</div>
             <br />
-            <div>
-              Website: HTML, CSS, PHP
-            </div>
+            <div>Website: HTML, CSS, PHP</div>
           </div>
           <div className="project-link">
             <a href="https://github.com/bingying0me/Website-Tailored-Specifically-for-HKPA">
-              <img className="project-logo" src="/images/github.png" alt="github" /> 
+              <img
+                className="project-logo"
+                src="/images/github.png"
+                alt="github"
+              />
             </a>
           </div>
         </div>
